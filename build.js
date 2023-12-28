90b2b06487e31dff0d2b80976d3b2c3682ec6acf
LeoX DEV <leoqonline@gmail.com>
2023-12-28 18:25:45 +0100

    Add license header to generated CSS files

--- build.js
+++ build.js
@@ -6,8 +6,10 @@ if (!fs.existsSync(__dirname + '/dist')) {
   fs.mkdirSync(__dirname + '/dist');
 };
 
+const license = '/*\n\n' + fs.readFileSync(__dirname + '/LICENSE', 'utf-8') + '\n*/\n\n';
+
 fs.readdirSync(__dirname + '/src/styles').forEach(file => {
   const result = sass.compile(__dirname + '/src/styles/' + file);
-  fs.writeFileSync(__dirname + '/dist/' + file.replace('.scss', '.css'), result.css);
-  fs.writeFileSync(__dirname + '/dist/' + file.replace('.scss', '.min.css'), csso.minify(result.css).css);
+  fs.writeFileSync(__dirname + '/dist/' + file.replace('.scss', '.css'), license + result.css);
+  fs.writeFileSync(__dirname + '/dist/' + file.replace('.scss', '.min.css'), license + csso.minify(result.css).css);
 });
